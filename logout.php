7f8c7b73ed041dcd2b6d5d71b11864344836e122
scottShams <scott123.staff.india@gmail.com>
2025-11-10 20:59:39 +0600

    Remove session and also cookie

--- logout.php
+++ logout.php
@@ -21,6 +21,12 @@ if (ini_get("session.use_cookies")) {
 // Destroy the session completely
 session_destroy();
 
+// Delete your custom cookies
+setcookie('user_email', '', time() - 3600, '/');
+setcookie('user_name', '', time() - 3600, '/');
+setcookie('user_income', '', time() - 3600, '/');
+
+
 // Optional: redirect to login or homepage
 header("Location: index.php");
 exit;
